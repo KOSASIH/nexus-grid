03a5238e8f6969905f3b0ecf1d0a288e538ed3df
KOSASIH <kosasihg88@gmail.com>
2024-07-19 13:22:24 +0700

    Create node.py

---
+++ src/backend/models/node.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Node(models.Model):
+    grid = models.ForeignKey('Grid', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
