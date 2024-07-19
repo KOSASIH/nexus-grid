8c4c4f437aebd06c71a35b1e564f704e9c3488f3
KOSASIH <kosasihg88@gmail.com>
2024-07-19 13:44:02 +0700

    Create Simulation.py

---
+++ src/backend/models/Simulation.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+class Simulation(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    grid = models.ForeignKey('Grid', on_delete=models.CASCADE)
+    nodes = ArrayField(models.IntegerField(), default=list)
+    edges = ArrayField(models.IntegerField(), default=list)
+    algorithm = models.ForeignKey('Algorithm', on_delete=models.CASCADE)
+    results = ArrayField(models.FloatField(), default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_nodes(self):
+        return Node.objects.filter(id__in=self.nodes)
+
+    def get_edges(self):
+        return Edge.objects.filter(id__in=self.edges)
+
+    def add_node(self, node):
+        self.nodes.append(node.id)
+        self.save()
+
+    def add_edge(self, edge):
+        self.edges.append(edge.id)
+        self.save()
+
+    def remove_node(self, node):
+        self.nodes.remove(node.id)
+        self.save()
+
+    def remove_edge(self, edge):
+        self.edges.remove(edge.id)
+        self.save()
